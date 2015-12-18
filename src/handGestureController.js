229ccafbbd39c39d60a1813e9d236ae5b720cbf7
dearshrewdwit <dearshrewdwit@gmail.com>
2015-12-18 10:23:03 +0000

    hand gesture controller all in one

--- src/handGestureController.js
+++ src/handGestureController.js
@@ -1,78 +1,111 @@
 var Leap = require("leapjs");
-
+var Sphero = require("sphero");
+var orb = Sphero("/dev/tty.Sphero-RGO-AMP-SPP", { timeout: 300});
 var options = { enableGestures: true };
-var direction;
-var speed;
+var direction = 0;
+var speed = 0;
+var moveCommands = {
+  "STRAIGHT AHEAD": 0,
+  "SMALL RIGHT": 30,
+  "MEDIUM RIGHT": 60,
+  "LARGE RIGHT": 90,
+  "REVERSE": 180,
+  "LARGE LEFT": 270,
+  "MEDIUM LEFT": 300,
+  "SMALL LEFT": 330
+};
+var speedCommands = {
+  "STOP": 0,
+  "SLOW": 20,
+  "MEDIUM": 50,
+  "FAST": 100
+};
 // Main Leap Loop
 var controller = new Leap.Controller(options);
 
-controller.on('connect', function() {
-  console.log('connected to leap motion');
-  setInterval(function() {
-    var frame = controller.frame();
-    for (var i = 0, len = frame.hands.length; i < len; i++) {
-      hand = frame.hands[i];
-      if(hand){
-        console.log(hand.type);
-        if (hand.type == 'left') {
-          handleLeft(hand);
-        } else if (hand.type == 'right') {
-          handleRight(hand);
+orb.connect(function(){
+  orb.color('yellow');
+  orb.setBackLed();
+    controller.on('connect', function() {
+      console.log('connected to leap motion');
+      setInterval(function() {
+        var frame = controller.frame();
+        for (var i = 0, len = frame.hands.length; i < len; i++) {
+          hand = frame.hands[i];
+          if(hand){
+            console.log(hand.type);
+            if (hand.type == 'left') {
+              handleLeft(hand);
+            } else if (hand.type == 'right') {
+              handleRight(hand);
+            }
+          }
+        }
+        console.log(speedCommands[speed]);
+        console.log(moveCommands[direction]);
+
+        orb.roll(speedCommands[speed], moveCommands[direction]);
+      }, 200);
+    });
+
+    controller.on('ready', function() {
+        console.log('ready');
+    });
+    controller.on('deviceStreaming', function() {
+        console.log('device connected');
+    });
+    controller.on('deviceStopped', function() {
+        console.log('device disconnected');
+    });
+
+
+    var handleRight = function(hand) {
+      var handRoll = hand.roll();
+      if(hand.pitch() > 0.75) {
+        orb.color('white');
+        direction = "REVERSE";
+      } else {
+          orb.color("green");
+          if(handRoll < -0.7) {
+            direction = 'LARGE RIGHT';
+          } else if(handRoll < -0.375) {
+            direction = 'MEDIUM RIGHT';
+          } else if(handRoll < -0.15 ) {
+            direction = 'SMALL RIGHT';
+          } else if(handRoll <= 0 && handRoll >= -0.15) {
+            direction = "STRAIGHT AHEAD";
+          } else if(handRoll > 0.65) {
+            direction = 'LARGE LEFT';
+          } else if(handRoll > 0.375) {
+            direction = 'MEDIUM LEFT';
+          } else if(handRoll > 0.1) {
+            direction = 'SMALL LEFT';
+          }
+        }
+      console.log('Direction: ', direction);
+      if (speed === "STOP") { console.log(speed); }
+    };
+
+    var handleLeft = function(hand) {
+      var handHeight = hand.palmPosition[1];
+      if(hand.grabStrength > 0.9) {
+        orb.color('red');
+        speed = "STOP";
+      } else {
+        if (handHeight > 250) {
+          speed = 'FAST';
+        } else if (handHeight > 130) {
+          speed = 'MEDIUM';
+        } else {
+          speed = 'SLOW';
         }
       }
-    }
-  }, 200);
+
+      console.log('Speed: ', speed);
+
+    };
+
+    controller.connect();
+    console.log('waiting for the leap motion to connect');
+
 });
-
-controller.on('ready', function() {
-    console.log('ready');
-});
-controller.on('deviceStreaming', function() {
-    console.log('device connected');
-});
-controller.on('deviceStopped', function() {
-    console.log('device disconnected');
-});
-
-
-var handleRight = function(hand) {
-  var handRoll = hand.roll();
-  if(hand.grabStrength > 0.9) {
-    direction = "STOP";
-  } else if(hand.pitch() > 0.75) {
-    direction = "REVERSE";
-  } else {
-      if(handRoll < -0.7) {
-        direction = 'LARGE RIGHT';
-      } else if(handRoll < -0.375) {
-        direction = 'MEDIUM RIGHT';
-      } else if(handRoll < -0.15 ) {
-        direction = 'SMALL RIGHT';
-      } else if(handRoll <= 0 && handRoll >= -0.15) {
-        direction = "STRAIGHT AHEAD";
-      } else if(handRoll > 0.65) {
-        direction = 'LARGE LEFT';
-      } else if(handRoll > 0.375) {
-        direction = 'MEDIUM LEFT';
-      } else if(handRoll > 0.1) {
-        direction = 'SMALL LEFT';
-      }
-    }
-
-  console.log('Direction: ', direction);
-};
-
-var handleLeft = function(hand) {
-  var handHeight = hand.palmPosition[1];
-  if (handHeight > 250) {
-    speed = 'FAST';
-  } else if (handHeight > 130) {
-    speed = 'MEDIUM';
-  } else {
-    speed = 'SLOW';
-  }
-  console.log('Speed: ', speed);
-};
-
-controller.connect();
-console.log('waiting for the leap motion to connect');
--- src/sphero.js
+++ src/sphero.js
@@ -1,22 +1,38 @@
-function Player() {
+"use strict";
+// var sphero = require("../");
+var orb, directions;
+
+
+function MovementCommands (orb){
+  this.orb = orb;
+  this.DEFAULT_SPEED = 50;
+  this.directions = {
+    FORWARD: 0,
+    SMALL_RIGHT: 45,
+    MEDIUM_RIGHT: 90,
+    LARGE_RIGHT: 135,
+    BACKWARD: 180,
+    LARGE_LEFT: 225,
+    MEDIUM_LEFT: 270,
+    SMALL_LEFT: 315
+  };
+  this.speeds = {
+    SLOW: 25,
+    MEDIUM: 50,
+    FAST: 100
+  };
+  this._setColor = function(color) {
+    this.orb.color(color);
+  };
 }
-Player.prototype.play = function(song) {
-  this.currentlyPlayingSong = song;
-  this.isPlaying = true;
-};
 
-Player.prototype.pause = function() {
-  this.isPlaying = false;
-};
-
-Player.prototype.resume = function() {
-  if (this.isPlaying) {
-    throw new Error("song is already playing");
-  }
-
-  this.isPlaying = true;
-};
-
-Player.prototype.makeFavorite = function() {
-  this.currentlyPlayingSong.persistFavoriteStatus(true);
+MovementCommands.prototype = {
+  stop: function() {
+    this.orb.stop();
+    this._setColor('red');
+  },
+  roll: function(speed, direction){
+    this.orb.roll(this.DEFAULT_SPEED, direction);
+    this._setColor('blue');
+  },
 };
