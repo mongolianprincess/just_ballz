df229b9d1431f28044d7d5c85f8ab7618dc049ec
dearshrewdwit <dearshrewdwit@gmail.com>
2016-01-05 19:40:32 +0000

    added grabstrength property to hand

--- spec/leapMotionSpec.js
+++ spec/leapMotionSpec.js
@@ -1,7 +1,8 @@
 describe('Steering System:', function(){
 
   beforeEach(function() {
-    orb = { roll: function(a,b){},
+    var hand;
+    orb = { roll: function(speed, heading){},
             disconnect: function(){}
           };
   });
@@ -9,37 +10,43 @@ describe('Steering System:', function(){
   describe('#handleRight', function() {
 
     it('upperLeftQuadrant', function() {
-      var hand = { palmPosition: [-50, null, -40] };
+      hand = { palmPosition: [-50, null, -40],
+               grabStrength: 0.8 };
       handleRight(hand);
       expect(heading).toEqual(308.6598082540901);
     });
 
     it('lowerLeftQuadrant', function() {
-      var hand = { palmPosition: [-50, null, 40] };
+      hand = { palmPosition: [-50, null, 40],
+               grabStrength: 0.8 };
       handleRight(hand);
       expect(heading).toEqual(231.34019174590992);
     });
 
     it('lowerRightQuadrant', function() {
-      var hand = { palmPosition: [50, null, 40] };
+      hand = { palmPosition: [50, null, 40],
+               grabStrength: 0.8} ;
       handleRight(hand);
       expect(heading).toEqual(128.65980825409008);
     });
 
     it('upperRightQuadrant', function() {
-      var hand = { palmPosition: [50, null, -40] };
+      hand = { palmPosition: [50, null, -40],
+               grabStrength: 0.8 };
       handleRight(hand);
       expect(heading).toEqual(51.34019174590991);
     });
 
     it('recognises a deadzone', function() {
-      var hand = { palmPosition: [0, null, 0] };
+      hand = { palmPosition: [0, null, 0],
+               grabStrength: 0.8 };
       handleRight(hand);
       expect(inDeadZone).toBeTruthy();
     });
 
     it('recognises when not in deadzone', function() {
-      var hand = { palmPosition: [50, null, -40] };
+      hand = { palmPosition: [50, null, -40],
+               grabStrength: 0.8 };
       calculateHeading(hand);
       expect(inDeadZone).toBeFalsy();
     });
