60a425a58a1a9becfe4fdcfe920e354db3020367
dearshrewdwit <dearshrewdwit@gmail.com>
2016-01-05 18:39:09 +0000

    testing now main handleRight function to give correct heading and recognise deadzone

--- spec/leapMotionSpec.js
+++ spec/leapMotionSpec.js
@@ -1,33 +1,48 @@
 describe('Steering System:', function(){
 
-  describe('#calculateHeading', function() {
+  beforeEach(function() {
+    speed = 80;
+    orb = { roll: function(a,b){},
+            disconnect: function(){}
+          };
+  });
 
-    beforeEach(function() {
-      var x, z, arctan, heading, inDeadZone;
-    });
+  describe('#handleRight', function() {
 
     it('upperLeftQuadrant', function() {
       var hand = { palmPosition: [-50, null, -40] };
-      calculateHeading(hand);
+      handleRight(hand);
       expect(heading).toEqual(308.6598082540901);
     });
 
     it('lowerLeftQuadrant', function() {
       var hand = { palmPosition: [-50, null, 40] };
-      calculateHeading(hand);
+      handleRight(hand);
       expect(heading).toEqual(231.34019174590992);
     });
 
     it('lowerRightQuadrant', function() {
       var hand = { palmPosition: [50, null, 40] };
-      calculateHeading(hand);
+      handleRight(hand);
       expect(heading).toEqual(128.65980825409008);
     });
 
     it('upperRightQuadrant', function() {
       var hand = { palmPosition: [50, null, -40] };
-      calculateHeading(hand);
+      handleRight(hand);
       expect(heading).toEqual(51.34019174590991);
+    });
+
+    it('recognises a deadzone', function() {
+      var hand = { palmPosition: [0, null, 0] };
+      handleRight(hand);
+      expect(inDeadZone).toBeTruthy();
+    });
+
+    it('recognises when not in deadzone', function() {
+      var hand = { palmPosition: [50, null, -40] };
+      calculateHeading(hand);
+      expect(inDeadZone).toBeFalsy();
     });
 
   });
