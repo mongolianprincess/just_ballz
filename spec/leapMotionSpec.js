7df2a28e69002d68b83a9be1922b8a5a4ddb4589
dearshrewdwit <dearshrewdwit@gmail.com>
2016-01-05 12:18:45 +0000

    an attempt at refactoring the steering system

--- spec/leapMotionSpec.js
+++ spec/leapMotionSpec.js
@@ -1,8 +1,21 @@
-describe('Leap Motion:', function(){
+describe('Steering System:', function(){
 
-  describe('#concatData', function(){
-    it('simplifies the data output from the leapmotion frame', function(){
-        expect(concatData('Hand', 'right')).toEqual('Hand: right');
+  // describe('#concatData', function(){
+  //   it('simplifies the data output from the leapmotion frame', function(){
+  //       expect(concatData('Hand', 'right')).toEqual('Hand: right');
+  //   });
+  // });
+
+  describe('Upper Left Quadrant', function() {
+
+    beforeEach(function(){
+      var x = -30;
+      var z = -40;
+    });
+
+    it('correctly translates palm position to a heading', function() {
+      var arctan = (Math.atan(Math.abs(x)/Math.abs(z))*180/Math.PI);
+
     });
   });
 });
--- src/steering_system.js
+++ src/steering_system.js
@@ -4,31 +4,48 @@ var orb = Sphero("/dev/tty.Sphero-RGO-AMP-SPP", { timeout: 300});
 var controller = new Leap.Controller();
 var counter = 0;
 var colliding = false;
+var x;
+var z;
+var arctan;
+var speed = 80;
 
 orb.connect(listen);
 
 function handleRight(hand) {
-  var x = hand.palmPosition[0];
-  var z = hand.palmPosition[2];
-  var arctan = (Math.atan(Math.abs(x)/Math.abs(z))*180/Math.PI);
-  console.log([x,z]);
 
-  if (x > -40 && x < 30 && z > -20 && z < 30) {
-    orb.roll(0,0);
-  } else {
-    if (x < 0 && z < 0) { orb.roll(80, (360 - arctan)); }
-    if (x < 0 && z > 0) { orb.roll(80, (180 + arctan)); }
-    if (x > 0 && z > 0) { orb.roll(80, (180 - arctan)); }
-    if (x > 0 && z < 0) { orb.roll(80, arctan); }
-  }
+  moveSphero(hand);
 
   if (hand.grabStrength > 0.9) {
     orb.roll(0,0);
     orb.disconnect(function() {
-      console.log("Now disconnected from Sphero");
     });
   }
+}
 
+function moveSphero(hand) {
+
+  calculateAngle(hand);
+
+  var inDeadZone = x > -40 && x < 30 && z > -20 && z < 30;
+  var upperLeftQuadrant = x < 0 && z < 0;
+  var lowerLeftQuadrant = x < 0 && z > 0;
+  var lowerRightQuadrant = x > 0 && z > 0;
+  var upperRightQuadrant = x > 0 && z < 0;
+
+  if (inDeadZone) {
+    orb.roll(0,0);
+  } else {
+    if (upperLeftQuadrant) { orb.roll(speed, (360 - arctan)); }
+    if (lowerLeftQuadrant) { orb.roll(speed, (180 + arctan)); }
+    if (lowerRightQuadrant) { orb.roll(speed, (180 - arctan)); }
+    if (upperRightQuadrant) { orb.roll(speed, arctan); }
+  }
+}
+
+function calculateAngle(hand) {
+  x = hand.palmPosition[0];
+  z = hand.palmPosition[2];
+  arctan = (Math.atan(Math.abs(x)/Math.abs(z))*180/Math.PI);
 }
 
 function listen() {
