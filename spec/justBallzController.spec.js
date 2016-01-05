d3651b17de4708812be620166ee5b51de603f499
ALRW <awerner1@googlemail.com>
2016-01-05 16:57:40 +0000

    added viewPane variable for alternate views

--- spec/justBallzController.spec.js
+++ spec/justBallzController.spec.js
@@ -20,13 +20,20 @@ describe('JustBallz Controller', function(){
         scope.connect();
         expect(scope.viewPane).toEqual(2);
       });
+
     });
 
+    describe("#connected",function(){
 
-    it('it has a connected function',function(){
-      expect(scope.connected).toBeDefined();
+      it('it has a connected function',function(){
+        expect(scope.connected).toBeDefined();
+      });
+
+      it('changes the view variable', function(){
+        scope.connected();
+        expect(scope.viewPane).toEqual(3);
+      });
     });
-
     it('it has a disconnect function', function(){
       expect(scope.disconnect).toBeDefined();
     });
--- src/justBallzController.js
+++ src/justBallzController.js
@@ -2,7 +2,7 @@ justBallz.controller('JustBallzController',['$scope', function($scope){
   $scope.heading = 'rotate(180deg)';
 
   $scope.connect = function(){
-
+    $scope.viewPane = 2;
   };
 
   $scope.connected = function(){
