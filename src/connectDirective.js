c1eb991272a2f093894773bbe74312ab6ad7bb24
ALRW <awerner1@googlemail.com>
2016-01-06 11:49:42 +0000

    Added welcome and connect custom element directives

---
+++ src/connectDirective.js
@@ -0,0 +1,6 @@
+justBallz.directive('connectPage', function(){
+  return {
+    restrict: 'E',
+    templateUrl: 'views/connect-page.html'
+  };
+});
---
+++ src/welcomeDirective.js
@@ -0,0 +1,6 @@
+justBallz.directive('welcomePage', function(){
+  return {
+    restrict: 'E',
+    templateUrl: 'views/welcome-page.html'
+  };
+});
